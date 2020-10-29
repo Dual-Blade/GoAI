a02914b8765993becd379de819ae976eea23db56
dual-blade <xysdual@163.com>
2020-10-29 14:45:20 +0800

    first commit

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module goai
 
 go 1.15
+
+require (
+	github.com/go-gota/gota v0.10.1 // indirect
+	gonum.org/v1/gonum v0.8.1 // indirect
+)
--- test.go
+++ test.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/go-gota/gota/dataframe"
 )
 
 func main() {
-	f, err := os.Open("d:/data/iris.csv")
+	f, err := os.Open("d:/gop/goai/data/iris.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	fmt.Println("hello, world!")
-
+	irisDF := dataframe.ReadCSV(f)
+	fmt.Println(irisDF)
 }
