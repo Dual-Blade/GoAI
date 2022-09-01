e5c411c8454c1a9e2a7860a9a1c031f9622ab2c6
0xshandy <shandy.eth@gmail.com>
2022-09-01 10:32:07 +0800

    init

--- dmbot/main.go
+++ dmbot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -18,47 +19,70 @@ func proxyClient() *http.Client {
 	return &http.Client{Transport: transport}
 }
 
-func CopyPhotoSend(bot *tgbotapi.BotAPI, update tgbotapi.Update, from int64, to int64) {
+func CopyPhotoSend(bot *tgbotapi.BotAPI, update tgbotapi.Update, from int64, to int64) error {
 	if update.Message != nil {
 		if update.Message.Photo != nil && update.Message.Chat.ID == from {
 			msg := tgbotapi.NewCopyMessage(to, update.Message.Chat.ID, update.Message.MessageID)
 			//msg := tgbotapi.NewPhoto(-642879405, update.Message.Text)
 			_, err := bot.Send(msg)
 			if err != nil {
-				log.Printf("Send copy photo fail [group name]:%s", update.Message.Chat.Title)
+				return fmt.Errorf("CopyPhotoSend fail [group name-%s]: %s", update.Message.Chat.Title, err)
 			}
 		}
 	}
+	return nil
 }
-func CopyUpiSend(bot *tgbotapi.BotAPI, update tgbotapi.Update, from int64, to int64) {
+
+func CopyUpiSend(bot *tgbotapi.BotAPI, update tgbotapi.Update, from int64, to int64) (err error) {
 	if update.Message != nil {
 		if update.Message.Chat.ID == from {
 			text := update.Message.Text
-			upiList := ExtractUPI(text, `[\w-]+@[\w]+`)
-			if len(upiList) == 0 {
-				return
+			upiList, err := ExtractUPI(text, `[\w-]+@[\w]+`)
+			if err != nil {
+				return fmt.Errorf("CopyUpiSend fail [group name-%s]: %s", update.Message.Chat.Title, err)
 			}
+			if len(upiList) == 0 {
+				return fmt.Errorf("未解析到UPI")
+			}
+			//发送upi列表到目标群，并统计成功数量
+			n := 0
 			for _, upi := range upiList {
 				msg := tgbotapi.NewMessage(to, upi)
-				_, err := bot.Send(msg)
-				if err != nil {
-					log.Printf("Send copy upi fail [group name]:%s", update.Message.Chat.Title)
+				if _, err = bot.Send(msg); err != nil {
+					log.Printf("发送消息失败-upi：%s", err)
+				} else {
+					n++
 				}
 			}
-			MergeSql(len(upiList))
+			//成功数量0
+			if n == 0 {
+				return fmt.Errorf("CopyUpiSend fail: 0")
+			}
 			// @user to recv upi to test
 			msg := tgbotapi.NewMessage(to, "@Sanju_Tiger")
-			_, err := bot.Send(msg)
-			if err != nil {
-				log.Printf("Send @user fail [group name]:%s", update.Message.Chat.Title)
+			if _, err = bot.Send(msg); err != nil {
+				log.Printf("发送消息失败-@user：%s", err)
+			}
+			//发送成功数量到目标群
+			msg = tgbotapi.NewMessage(from, fmt.Sprintf("upi 发送成功，数量：%d", n))
+			if _, err = bot.Send(msg); err != nil {
+				log.Printf("发送消息失败-upi数量：%s", err)
+			}
+			//储存统计数量到mysql
+			if err = MergeSql(n); err != nil {
+				return fmt.Errorf("CopyUpiSend fail: %s", err.Error())
 			}
 		}
 	}
+	return nil
 }
 
-func ExtractUPI(text string, p string) []string {
-	r := regexp.MustCompile(p)
-	return r.FindAllString(text, -1)
+func ExtractUPI(text string, p string) ([]string, error) {
+	r, err := regexp.Compile(p)
+	if err != nil {
+		return []string{}, fmt.Errorf("ExtractUPI fail: %s", err)
+	}
+	return r.FindAllString(text, -1), nil
 }
 
 func main() {
@@ -78,8 +102,14 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		// UPIGBP:-633478699  jiang:-665365433
-		CopyUpiSend(bot, update, -642879405, -749897072)
-		CopyPhotoSend(bot, update, -749897072, -642879405)
+		// UPIGBP:-633478699 -749897072  jiang:-665365433 -642879405
+		err := CopyUpiSend(bot, update, -642879405, -749897072)
+		if err != nil {
+			log.Println(err)
+		}
+		err = CopyPhotoSend(bot, update, -749897072, -642879405)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
--- dmbot/sql.go
+++ dmbot/sql.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/golang-sql/civil"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"time"
@@ -12,31 +13,42 @@ type UpiCount struct {
 	Count int
 }
 
-func MergeSql(c int) {
+func MergeSql(c int) (err error) {
 	db, err := gorm.Open("mysql", "root:suez623810@(127.0.0.1:3306)/dm?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
-		panic(err)
+		return
 	}
 	defer func(db *gorm.DB) {
-		err := db.Close()
-		if err != nil {
-			fmt.Println(err)
+		closeErr := db.Close()
+		if err == nil && closeErr != nil {
+			err = fmt.Errorf("MergeSql fail: close %s", closeErr)
 		}
 	}(db)
 
 	//创建表 自动迁移 （把结构体和数据库表进行对应)
-	if err := db.AutoMigrate(&UpiCount{}); err != nil {
-		fmt.Println(err)
+	db.AutoMigrate(&UpiCount{})
+	if db.Error != nil {
+		err = fmt.Errorf("MergeSql fail: %s", db.Error.Error())
 	}
 
 	u := &UpiCount{}
-	db.Model(&u).First(&u)
-	fmt.Println(u)
+	today := civil.DateOf(time.Now()).String()
+	db.Model(&u).Where("date = ?", today).First(&u)
+	if db.Error != nil {
+		err = fmt.Errorf("MergeSql fail: %s", db.Error.Error())
+	}
+	//如果查询不到该日期
 	if !u.Date.IsZero() {
-		db.Model(&u).Update("count", u.Count+c)
+		db.Model(&u).Where("date = ?", today).Update("count", u.Count+c)
+
 	} else {
 		//创建数据行
 		u := UpiCount{Date: time.Now(), Count: c}
 		db.Model(&u).Create(&u)
 	}
+
+	if db.Error != nil {
+		err = fmt.Errorf("MergeSql fail: %s", db.Error.Error())
+	}
+	return
 }
